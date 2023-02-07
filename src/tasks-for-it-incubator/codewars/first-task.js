767063990c5fd3a318457d9c6b24ca19a5ac7fb3
Aizhan Rakhmetova <aizhan.rakhmetova@nu.edu.kz>
2023-02-07 17:47:11 +0500

    Revert "first-task on checking perfect square-1"
    This reverts commit 11b45f590d127e9c722d0f96f8149e8b30eb78ec.

--- src/tasks-for-it-incubator/codewars/first-task.js
+++ src/tasks-for-it-incubator/codewars/first-task.js
@@ -1,6 +1,6 @@
 // Checking if the given number is a perfect square
 
-function isSquareFunction(n) {
+function isSquare(n) {
     for (let i = 0; i * i <= n; i++) {
         if (i * i === n) {
             return true;
