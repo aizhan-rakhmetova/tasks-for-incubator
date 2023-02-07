11b45f590d127e9c722d0f96f8149e8b30eb78ec
Aizhan Rakhmetova <aizhan.rakhmetova@nu.edu.kz>
2023-02-07 17:20:01 +0500

    first-task on checking perfect square-1

--- src/tasks-for-it-incubator/codewars/first-task.js
+++ src/tasks-for-it-incubator/codewars/first-task.js
@@ -1,6 +1,6 @@
 // Checking if the given number is a perfect square
 
-function isSquare(n) {
+function isSquareFunction(n) {
     for (let i = 0; i * i <= n; i++) {
         if (i * i === n) {
             return true;
