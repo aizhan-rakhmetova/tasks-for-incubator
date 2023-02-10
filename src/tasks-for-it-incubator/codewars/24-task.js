bf2cdb409647ae15e271e965000dfde791c67e6f
Aizhan Rakhmetova <aizhan.rakhmetova@nu.edu.kz>
2023-02-10 14:39:47 +0500

    24-task

---
+++ src/tasks-for-it-incubator/codewars/24-task.js
@@ -0,0 +1,11 @@
+//Cat years, Dog years
+var humanYearsCatYearsDogYears = function(humanYears) {
+    // Your code here!
+    let catYears = 0;
+    let dogYears = 0;
+    for (let i = 1; i <= humanYears; i++) {
+        catYears += (i == 1) ? 15 : (i == 2) ? 9 : 4;
+        dogYears += (i == 1) ? 15 : (i == 2) ? 9 : 5;
+    }
+    return [humanYears, catYears, dogYears];
+}
