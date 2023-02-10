980c6d39d9df86818345fdc469741b688263b77f
Aizhan Rakhmetova <aizhan.rakhmetova@nu.edu.kz>
2023-02-10 14:04:53 +0500

    14-task

---
+++ src/tasks-for-it-incubator/codewars/14-task.js
@@ -0,0 +1,7 @@
+//Filter out the geese
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(bird => !geese.includes(bird));
+    // return an array containing all of the strings in the input array except those that match strings in geese
+};
